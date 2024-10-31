6e4ff6bd5b8036268a42cc79b4cba7a89f5847ba
levkirillov <165016957+levkirillov@users.noreply.github.com>
2024-10-31 18:23:58 +0500

    Update rednet_serv.lua

--- rednet_serv.lua
+++ rednet_serv.lua
@@ -44,11 +44,14 @@ local hostFile = fs.open('hosts.json', 'r')
 local hosts = textutils.unserializeJSON(hostFile.readAll())
 hostFile.close()
 
+-- Set up admin ID (hardcoded here for example, you could set this differently)
+local adminID = 1
+
 -- Main server loop
 while true do
-  local id, msg = rednet.receive()
-  if id then
-    print('Received from ID:', id)
+  local senderID, msg = rednet.receive()
+  if senderID then
+    print('Received from ID:', senderID)
     print('Message:', msg)
     
     -- Split message into command and arguments
@@ -56,36 +59,67 @@ while true do
     local command = parts[1]
     local arg1 = parts[2]
     local arg2 = parts[3]
+    local arg3 = parts[4] == "true"
 
-    -- Process command with arguments directly
     if command == "add" and arg1 and arg2 then
-      hosts[arg1] = arg2  -- Add entry to hosts (e.g., hostname = IP)
-
+      -- Add entry to hosts with privacy and owner info
+      hosts[arg1] = { ip = arg2, private = arg3, ownerID = senderID }
+      
       -- Save updated hosts data back to 'hosts.json'
       local hostFile = fs.open('hosts.json', 'w')
       hostFile.write(textutils.serializeJSON(hosts))
       hostFile.close()
       
-      print("Added host:", arg1, "with IP:", arg2)
-    
-    elseif command == "remove" and arg1 then
-      hosts[arg1] = nil  -- Remove entry from hosts
+      rednet.send(senderID, "Host added: " .. arg1)
+      print("Added host:", arg1, "with IP:", arg2, "private:", tostring(arg3))
 
-      -- Save updated hosts data
-      local hostFile = fs.open('hosts.json', 'w')
-      hostFile.write(textutils.serializeJSON(hosts))
-      hostFile.close()
-      
-      print("Removed host:", arg1)
-    
-    elseif command == "list" then
-      print("Current hosts list:")
-      for hostname, ip in pairs(hosts) do
-        print(hostname, ":", ip)
+    elseif command == "remove" and arg1 then
+      -- Check if the host exists and the sender has permission to delete
+      local host = hosts[arg1]
+      if host and (host.ownerID == senderID or senderID == adminID) then
+        hosts[arg1] = nil  -- Remove entry from hosts
+
+        -- Save updated hosts data
+        local hostFile = fs.open('hosts.json', 'w')
+        hostFile.write(textutils.serializeJSON(hosts))
+        hostFile.close()
+        
+        rednet.send(senderID, "Host removed: " .. arg1)
+        print("Removed host:", arg1)
+      else
+        rednet.send(senderID, "Permission denied or host not found.")
       end
-    
+
+    elseif command == "list" then
+      -- Collect only public hostnames
+      local publicHosts = {}
+      for hostname, data in pairs(hosts) do
+        if not data.private then
+          table.insert(publicHosts, hostname)
+        end
+      end
+      -- Send list of public hosts
+      rednet.send(senderID, "Public hosts: " .. textutils.serialize(publicHosts))
+      print("Sent public hosts list to ID:", senderID)
+
+    elseif command == "get" and arg1 then
+      -- Check if the host exists and send back its IP
+      local host = hosts[arg1]
+      if host then
+        rednet.send(senderID, "IP of " .. arg1 .. ": " .. host.ip)
+      else
+        rednet.send(senderID, "Host not found: " .. arg1)
+      end
+
     else
-      print("Invalid command or arguments:", command, arg1, arg2)
+      -- Send usage instructions if command is unrecognized
+      local usage = "Commands:\n"
+        .. "add <name> <IP> <private(true/false)>\n"
+        .. "remove <name>\n"
+        .. "list\n"
+        .. "get <name>"
+      rednet.send(senderID, "Invalid command.\n" .. usage)
+      print("Sent usage instructions to ID:", senderID)
     end
   end
 end
