075816d0963ed586102a35d2b0b2f284aa19a349
levkirillov <165016957+levkirillov@users.noreply.github.com>
2024-10-31 16:51:00 +0500

    Update rednet_serv.lua

--- rednet_serv.lua
+++ rednet_serv.lua
@@ -4,36 +4,36 @@ if not (fs.exists('port.cfg')) then
   if (tonumber(port)) < 0 or (tonumber(port)) > 65535 then
     file2 = exit()
   end
-  file2 = fs.open('port.cfg',w)
+  file2 = fs.open('port.cfg','w')
   file2.write(port)
   file2.close()
 end
-readp = fs.open('port.cfg',r)
+readp = fs.open('port.cfg','r')
 rport = readp.readLine()
 readp.close()
 rednet.open("top")
 if not (fs.exists('hosts.json')) then
-  h = fs.open('hosts.json',w)
+  h = fs.open('hosts.json','w')
   h.write('{}')
   h.close()
 end
-hostfile = fs.open('hosts.json',r)
+hostfile = fs.open('hosts.json','r')
 hosts = textutils.unserializeJSON(hostfile.readAll())
 
 while true do
-  d = rednet.receive()
-  if d[0] then
+  id,msg = rednet.receive()
+  if id then
     print('id')
-    print(d[0])
+    print(id)
     print('msg')
-    print(d[1])
+    print(msg)
     if last_c then
       if last_c == 'add' then
       end
       last_c = nil
     end
     if not last_c then
-      last_c = d[1]
+      last_c = msg
     end
   end
 end
